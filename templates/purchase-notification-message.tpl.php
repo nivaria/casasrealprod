2d44919beea8e6a4389c69b039babce2c552d1df
dbrouard <diego@net-monsters.com>
2012-11-14 10:44:20 +0000

    tables email template admin toolbar style was putting a margin on top

--- templates/purchase-notification-message.tpl.php
+++ templates/purchase-notification-message.tpl.php
@@ -1,64 +1,88 @@
-<table border="0" cellpadding="0" cellspacing="0" >
-  <tr>
-    <td height="24" >&nbsp;</td>
-  </tr>
-  <tr>
-    <td width="620" ><?php print $info ?></td>
-  </tr>
-</table>
-<table border="0" cellpadding="0" cellspacing="0" >
-  <tr>
-    <td valign="top" height="24">&nbsp;</td>
-  </tr>
-</table>
-<table border="0" cellpadding="16" cellspacing="0" >
-  <tr>
-    <td valign="top" bgcolor="#fff" width="588" ><?php print $summary ?></td>
-  </tr>
-</table>
-<table border="0" cellpadding="0" cellspacing="0" >
-  <tr>
-    <td valign="top" height="24">&nbsp;</td>
-  </tr>
-</table>
-<table border="0" cellpadding="0" cellspacing="0" >
-  <tr>
-    <td valign="top" bgcolor="#fff" cellpadding="0" width="24" >&nbsp;</td>
-    <td valign="top" bgcolor="#fff" cellpadding="0" width="250" ><?php print $block_remember ?></td>
-    <td valign="top" bgcolor="#fff" cellpadding="0" width="24" >&nbsp;</td>
-    <td valign="top" cellpadding="0" width="24" >&nbsp;</td>
-    <td valign="top" bgcolor="#fff" cellpadding="0" width="24" >&nbsp;</td>
-    <td valign="top" bgcolor="#fff" cellpadding="0" width="250" ><?php print $block_share ?></td>
-    <td valign="top" bgcolor="#fff" cellpadding="0" width="24" >&nbsp;</td>
-  </tr>
-</table>
-<table border="0" cellpadding="0" cellspacing="0" >
-  <tr>
-    <td valign="top" height="24">&nbsp;</td>
-  </tr>
-</table>
-<table border="0" cellpadding="0" cellspacing="0" >
-  <tr>
-    <td valign="top" bgcolor="#fff" cellpadding="0" width="24" >&nbsp;</td>
-    <td valign="top" bgcolor="#fff" width="250" ><?php print $block_contact ?></td>
-    <td valign="top" bgcolor="#fff" cellpadding="0" width="24" >&nbsp;</td>
-    <td valign="top" cellpadding="0" width="24" >&nbsp;</td>
-    <td valign="top" bgcolor="#fff" cellpadding="0" width="24" >&nbsp;</td>
-    <td valign="top" bgcolor="#fff" cellpadding="0" width="250" >
-      <table border="0" cellpadding="0" cellspacing="0" >
-        <tr>
-          <td valign="top" bgcolor="#fff" ><?php print $block_location ?></td>
-        </tr>
-        <tr>
-          <td valign="top" bgcolor="#fff" ><?php print $block_extra ?></td>
-        </tr>
-      </table>
-    </td>
-    <td valign="top" bgcolor="#fff" cellpadding="0" width="24" >&nbsp;</td>
-  </tr>
-</table>
-<table border="0" cellpadding="0" cellspacing="0" >
-  <tr>
-    <td valign="top" height="24">&nbsp;</td>
-  </tr>
-</table>
+                                <tr>
+                                    <td valign="top" class="bodycontent" style="border-collapse: collapse;">
+                                        <table border="0" cellpadding="0" cellspacing="0">
+                                            <tr>
+                                                <td height="24" style="border-collapse: collapse;">&nbsp;</td>
+                                            </tr>
+                                            <tr>
+                                                <td width="620" style="font-size:16px;border-collapse: collapse;"><?php print $info ?></td>
+                                            </tr>
+                                        </table>
+                                        <table border="0" cellpadding="0" cellspacing="0">
+                                            <tr>
+                                                <td valign="top" height="24" style="border-collapse: collapse;">&nbsp;</td>
+                                            </tr>
+                                        </table>
+                                        <table border="0" cellpadding="16" cellspacing="0">
+                                            <?php print $summary ?>
+                                        </table>
+                                        <table border="0" cellpadding="0" cellspacing="0">
+                                            <tr>
+                                                <td valign="top" height="24" style="border-collapse: collapse;">&nbsp;</td>
+                                            </tr>
+                                        </table>
+                                        <table border="0" cellpadding="0" cellspacing="0">
+                                            <tr>
+                                                <td valign="top" bgcolor="#fff" cellpadding="0" width="298" style="border-collapse: collapse;">
+                                                    <?php print $block_remember ?>
+                                                </td>
+                                                <td valign="top" bgcolor="#ededed" cellpadding="0" width="24" style="border-collapse: collapse;">&nbsp;</td>
+                                                <td valign="top" bgcolor="#fff" cellpadding="0" width="298" style="border-collapse: collapse;">
+                                                    <?php print $block_share ?>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                        <table border="0" cellpadding="0" cellspacing="0">
+                                            <tr>
+                                                <td valign="top" height="24" style="border-collapse: collapse;">&nbsp;</td>
+                                            </tr>
+                                        </table>
+                                        <table border="0" cellpadding="0" cellspacing="0">
+                                            <tr>
+                                                <td valign="top" bgcolor="#fff" width="298" style="border-collapse: collapse;">
+                                                    <table border="0" cellpadding="0" cellspacing="0">
+                                                        <tr>
+                                                            <td>
+                                                                <?php print $block_contact ?>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                                <td valign="top" bgcolor="#ededed" cellpadding="0" width="24" style="border-collapse: collapse;">&nbsp;</td>
+                                                <td valign="top" cellpadding="0" width="298" style="border-collapse: collapse;">
+                                                    <table border="0" cellpadding="0" cellspacing="0">
+                                                        <tr>
+                                                            <td valign="top" bgcolor="#fff" style="border-collapse: collapse;">
+                                                                <?php print $block_location ?>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>
+                                                                <table border="0" cellpadding="0" cellspacing="0">
+                                                                    <tr>
+                                                                        <td valign="top" style="text-align:center">
+                                                                            <a target="_blank" href="https://maps.google.com/maps?daddr=28.2407197,-16.4334325&amp;ll=28.2407197,-16.4334325&amp;t=z&amp;z=15" style="color:#666;text-decoration:none;"><img style="vertical-align:middle;" src="images_email/email_link_icon.png"/></a>
+                                                                        </td>
+                                                                    </tr>
+                                                                </table>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td valign="top" bgcolor="#ededed" height="24" style="border-collapse: collapse;">&nbsp;</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td valign="top" bgcolor="#fff" style="border-collapse: collapse;">
+                                                                <?php print $block_extra ?>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                        <table border="0" cellpadding="0" cellspacing="0">
+                                            <tr>
+                                                <td valign="top" height="24" style="border-collapse: collapse;">&nbsp;</td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
