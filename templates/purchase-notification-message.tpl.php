d55bf6ea274385ed4807f4590169e9aed968c5bb
rmunglez <rmglez@anaga-mobile.(none)>
2012-11-27 18:18:20 +0000

    correo de notificación

--- templates/purchase-notification-message.tpl.php
+++ templates/purchase-notification-message.tpl.php
@@ -25,13 +25,13 @@
                                                             <td bgcolor="#fff" width="452" valign="top" >
                                                                 <div style="font-size:20px;color:#1f83b1;margin-bottom:8px;" ><?php print $room['title']; ?></div>
                                                                 <div style="font-size:12px;margin-bottom:8px;">
-                                                                  <img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => 'en')); ?>"/>&nbsp;<?php print t('Arrival date'); ?>: <?php print $room['arrival_date']; ?>
+                                                                  <img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => $variables['lang'])); ?>"/>&nbsp;<?php print t('Arrival date'); ?>: <?php print $room['arrival_date']; ?>
                                                                 </div>
                                                                 <div style="font-size:12px;margin-bottom:8px;">
-                                                                  <img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => 'en')); ?>"/>&nbsp;<?php print t('Departure date'); ?>: <?php print $room['departure_date']; ?>
+                                                                  <img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => $variables['lang'])); ?>"/>&nbsp;<?php print t('Departure date'); ?>: <?php print $room['departure_date']; ?>
                                                                 </div>
                                                                 <div style="font-size:12px;margin-bottom:8px;">
-                                                                  <img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => 'en')); ?>"/>&nbsp;<?php print t('Occupants'); ?>: <?php print $room['occupants']; ?>
+                                                                  <img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => $variables['lang'])); ?>"/>&nbsp;<?php print t('Occupants'); ?>: <?php print $room['occupants']; ?>
                                                                 </div>
                                                             </td>
                                                             <td bgcolor="#fff" width="136" valign="top" >
@@ -88,8 +88,8 @@
                                                                 <table border="0" cellpadding="0" cellspacing="0">
                                                                     <tr>
                                                                         <td width="266" valign="top">
-                                                                            <div style="font-size:14px;margin-bottom:8px;"><img style="vertical-align:middle;" src="<?php print url($directory.'/images/email_facebook_icon.png', array('absolute' => TRUE, 'language' => 'en')); ?>"/>&nbsp;&nbsp;&nbsp;<a style="text-decoration:none;color:#1f83b1;white-space:nowrap;" href="http://www.facebook.com/sharer.php?u=http://www.lascasasdelcaminoreal.com" style="color:#666;text-decoration:none;"><?php print t('Share in your wall'); ?></a></div>
-                                                                            <div style="font-size:14px;margin-bottom:8px;"><img style="vertical-align:middle;" src="<?php print url($directory.'/images/email_twitter_icon.png', array('absolute' => TRUE, 'language' => 'en')); ?>"/>&nbsp;&nbsp;&nbsp;<a style="text-decoration:none;color:#1f83b1;white-space:nowrap;" href="http://twitter.com/share?text=Casas%20del%20Camino%20Real&amp;url=http://www.lascasasdelcaminoreal.com" style="color:#666;text-decoration:none;"><?php print t('Publish in Twitter'); ?></a></div>
+                                                                            <div style="font-size:14px;margin-bottom:8px;"><img style="vertical-align:middle;" src="<?php print url($directory.'/images/email_facebook_icon.png', array('absolute' => TRUE, 'language' => $variables['lang'])); ?>"/>&nbsp;&nbsp;&nbsp;<a style="text-decoration:none;color:#1f83b1;white-space:nowrap;" href="http://www.facebook.com/sharer.php?u=http://www.lascasasdelcaminoreal.com" style="color:#666;text-decoration:none;"><?php print t('Share in your wall'); ?></a></div>
+                                                                            <div style="font-size:14px;margin-bottom:8px;"><img style="vertical-align:middle;" src="<?php print url($directory.'/images/email_twitter_icon.png', array('absolute' => TRUE, 'language' => $variables['lang'])); ?>"/>&nbsp;&nbsp;&nbsp;<a style="text-decoration:none;color:#1f83b1;white-space:nowrap;" href="http://twitter.com/share?text=Casas%20del%20Camino%20Real&amp;url=http://www.lascasasdelcaminoreal.com" style="color:#666;text-decoration:none;"><?php print t('Publish in Twitter'); ?></a></div>
                                                                         </td>
                                                                     </tr>
                                                                 </table>
@@ -118,8 +118,8 @@
                                                                         <td bgcolor="#fff" valign="top" style="text-align:center;">
                                                                             <p><?php print t('Telephone'); ?>: (0034) 922 000 000</p>
                                                                             <p><?php print t('We speak english and spanish'); ?></p>
-                                                                            <img typeof="foaf:Image" src="<?php print url(variable_get('file_public_path', conf_path() . '/files').'/bloques/bloque_mita_nicolas.jpg', array('absolute' => TRUE, 'language' => 'en')); ?>" width="184" height="137" alt="" style="border: 0;height: auto;line-height: 100%;outline: none;text-decoration: none;">
-                                                                            <p><a href="<?php print url('contacto', array('absolute' => TRUE)); ?>" style="color:#666;text-decoration:none;"><img src="<?php print url($directory.'/images/email_contact_icon.png', array('absolute' => TRUE, 'language' => 'en')); ?>"/></a></p>
+                                                                            <img typeof="foaf:Image" src="<?php print url(variable_get('file_public_path', conf_path() . '/files').'/bloques/bloque_mita_nicolas.jpg', array('absolute' => TRUE, 'language' => $variables['lang'])); ?>" width="184" height="137" alt="" style="border: 0;height: auto;line-height: 100%;outline: none;text-decoration: none;">
+                                                                            <p><a href="<?php print url('contacto', array('absolute' => TRUE)); ?>" style="color:#666;text-decoration:none;"><img src="<?php print url($directory.'/images/email_contact_icon.png', array('absolute' => TRUE, 'language' => $variables['lang'])); ?>"/></a></p>
                                                                         </td>
                                                                     </tr>
                                                                 </table>
@@ -150,7 +150,7 @@
                                                                 <table border="0" cellpadding="0" cellspacing="0">
                                                                     <tr>
                                                                         <td valign="top" style="text-align:center">
-                                                                            <a target="_blank" href="https://maps.google.com/maps?daddr=28.2407197,-16.4334325&amp;ll=28.2407197,-16.4334325&amp;t=z&amp;z=15" style="color:#666;text-decoration:none;"><img style="vertical-align:middle;" src="<?php print url($directory.'/images/email_link_icon.png', array('absolute' => TRUE, 'language' => 'en')); ?>"/></a>
+                                                                            <a target="_blank" href="https://maps.google.com/maps?daddr=28.2407197,-16.4334325&amp;ll=28.2407197,-16.4334325&amp;t=z&amp;z=15" style="color:#666;text-decoration:none;"><img style="vertical-align:middle;" src="<?php print url($directory.'/images/email_link_icon.png', array('absolute' => TRUE, 'language' => $variables['lang'])); ?>"/></a>
                                                                                                                                                 </td>
                                                                     </tr>
                                                                 </table>
@@ -167,9 +167,9 @@
                                                                     </tr>
                                                                     <tr>
                                                                         <td bgcolor="#fff" valign="top" >
-                                                                            <div style="font-size:12px;margin-bottom:8px;text-transform:uppercase;"><img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => 'en')); ?>"/>&nbsp;Servicios que puedes contratar</div>
-                                                                            <div style="font-size:12px;margin-bottom:8px;text-transform:uppercase;"><img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => 'en')); ?>"/>&nbsp;Actividades que puedes realizar</div>
-                                                                            <div style="font-size:12px;margin-bottom:8px;text-transform:uppercase;"><img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => 'en')); ?>"/>&nbsp;¿Organizas un evento?</div>                                                              
+                                                                            <div style="font-size:12px;margin-bottom:8px;text-transform:uppercase;"><img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => $variables['lang'])); ?>"/>&nbsp;Servicios que puedes contratar</div>
+                                                                            <div style="font-size:12px;margin-bottom:8px;text-transform:uppercase;"><img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => $variables['lang'])); ?>"/>&nbsp;Actividades que puedes realizar</div>
+                                                                            <div style="font-size:12px;margin-bottom:8px;text-transform:uppercase;"><img src="<?php print url($directory.'/images/email_check_icon.png', array('absolute' => TRUE, 'language' => $variables['lang'])); ?>"/>&nbsp;¿Organizas un evento?</div>                                                              
                                                                         </td>
                                                                     </tr>
                                                                 </table>
